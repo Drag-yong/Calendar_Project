6636804b87e22aec62ab2627104865e2bf408d7f
Drag-yong <ypark272@wisc.edu>
2019-09-10 11:49:09 +0900

    Added methods

--- src/CalendarPrinter.java
+++ src/CalendarPrinter.java
@@ -1,5 +1,3 @@
-import javax.print.DocFlavor;
-
 public class CalendarPrinter {
     private final static String[] DAYS_OF_WEEK = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
     private final static String[] MONTHS_OF_YEAR = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP",
@@ -15,9 +13,7 @@ public class CalendarPrinter {
      * @return number of centuries in the specified year
      */
     public static int getCentury(String year) {
-        int yearInt = Integer.parseInt(year);
-        int century = (int) Math.floor(yearInt / 100);
-        return century;
+        return (int) Math.floor(Integer.parseInt(year) / 100);
     }
 
     /**
@@ -30,26 +26,25 @@ public class CalendarPrinter {
      * @return number of years since first year in the current century
      */
     public static int getYearWithinCentury(String year) {
-        int century = getCentury(year) * 100; // get century and multiplied by 100, or first year's century
-        int specifiedYear = Integer.parseInt(year) - century;
-        return specifiedYear;
+        return Integer.parseInt(year) % 100;
     }
 
     /**
      * This method computes whether the specified year is a leap year or not.
      *
-     * @param yearString is the year that is being checked for leap-year-ness String
-     *                   must contain the digits of a single non-negative int for
+     * @param yearString is the year that is being checked for leap-years String
+     *                   must contain the digits of a single non-negativt for
      *                   year.
      * @return true when the specified year is a leap year, and false otherwise
      */
     public static boolean getIsLeapYear(String yearString) {
         // Note implementation tips in Appendix I below.
-//		if (year is not divisible by 4) then (it is a common year)
-//		else if (year is not divisible by 100) then (it is a leap year)
-//		else if (year is not divisible by 400) then (it is a common year)
-//		else (it is a leap year)
-        // Pseudocode for the leap year
+/////////////////////////Pseudocode for the leap year/////////////////////////
+//		if (year is not divisible by 4) then (it is a common year)          //
+//		else if (year is not divisible by 100) then (it is a leap year)     //
+//		else if (year is not divisible by 400) then (it is a common year)   //
+//		else (it is a leap year)                                            //
+//////////////////////////////////////////////////////////////////////////////
         int getYear = Integer.parseInt(yearString);
 
         if (getYear % 4 != 0) {
@@ -107,24 +102,18 @@ public class CalendarPrinter {
         boolean isLeapYear = getIsLeapYear(year); // check if it is leap year or not
         int monthIndex = getMonthIndex(month); // get a value = Mon - 1;
 
-        boolean check31 = monthIndex == 0 || monthIndex == 2 || monthIndex == 4 || monthIndex == 6 || monthIndex == 7 || monthIndex == 9 || monthIndex == 11;
-        if (isLeapYear) { // check if it is a leap year or not
-            if (check31) {
-                return 31;
-            } else if (monthIndex == 1) { // check if it is February
+
+        if (monthIndex == 0 || monthIndex == 2 || monthIndex == 4 || monthIndex == 6 || monthIndex == 7 || monthIndex == 9 || monthIndex == 11) {
+            return 31;
+        } else if (monthIndex == 1) { // check if it is February
+            if (isLeapYear) // check if it is a leap year or not
                 return 29; // because of leap year
-            } else {
-                return 30;
-            }
+            else
+                return 28;
         } else {
-            if (check31) {
-                return 31;
-            } else if (monthIndex == 1) { // check if it is February
-                return 28; // because of common year
-            } else {
-                return 30;
-            }
+            return 30;
         }
+
     }
 
     /**
@@ -140,7 +129,23 @@ public class CalendarPrinter {
      */
     public static int getFirstDayOfWeekInMonth(String month, String year) {
         // Note implementation tips in Appendix I below.
-        return 0;
+        int q = getNumberOfDaysInMonth(month, year); // Day of the month
+
+        int m; // The number of days in the month,  3 = March, 4 = April, 5 = May, ..., 14 = February
+        int checkMonthIndex = getMonthIndex(month);
+        if (checkMonthIndex < 2)
+            m = checkMonthIndex + 13; // January = 13, February = 14
+        else
+            m = checkMonthIndex + 1; // March = 3, April = 4, ...
+
+        int k = getYearWithinCentury(year); // the year of the century
+        int j = getCentury(year); // the zero based century
+
+        System.out.printf("q=%d, m=%d, k=%d, j=%d%n", q, m, k, j);
+
+        int daysOfTheWeek = (q + Math.floorDiv(13 * (m + 1), 5) + k + Math.floorDiv(k, 4) + Math.floorDiv(j, 4) + 5 * j) % 7;
+        // 0 = Saturday, 1 = Sunday, 2 = Monday, ..., 6 = Friday
+        return daysOfTheWeek;
     }
 
     /**
@@ -164,5 +169,15 @@ public class CalendarPrinter {
      */
     public static String[][] generateCalendar(String month, String year) {
         return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getFirstDayOfWeekInMonth("September", "2019"));
+        System.out.println(getFirstDayOfWeekInMonth("August", "2019"));
+        System.out.println(getFirstDayOfWeekInMonth("October", "2019"));
+        System.out.println(getFirstDayOfWeekInMonth("November", "2019"));
+        System.out.println(getFirstDayOfWeekInMonth("December", "2019"));
+        System.out.println(getFirstDayOfWeekInMonth("January", "2019"));
+        System.out.println(getFirstDayOfWeekInMonth("March", "2019"));
     }
 }
